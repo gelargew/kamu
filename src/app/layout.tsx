bee97dccfcc373d65e54d669dec12ef8c98d81b6
Gelar Rustiawan <gelargew@gmail.com>
2024-01-25 13:05:52 +0700

    readme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
-  title: "Me App",
+  title: "Kamu",
   description: "Its you and me",
 };
 
